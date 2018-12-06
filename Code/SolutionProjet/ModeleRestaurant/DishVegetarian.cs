5fefaf3bfba6d12224e378bff4d7bfaeb9f5028e
Xavier gistau <xavier.gistau@viacesi.fr>
2018-12-06 15:09:56 +0100

    Adding few classes : - AbstractDish and its relative dishes - IPlatFactory and its relative factories

---
+++ Code/SolutionProjet/ModeleRestaurant/DishVegetarian.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeleRestaurant
+{
+    public class DishVegetarian : AbstractDish
+    {
+      
+
+        DishVegetarian()
+        {
+            this.Name="toto";
+            
+            
+            
+        }
+
+    }
+}
