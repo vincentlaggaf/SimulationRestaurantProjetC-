d58d132d46d52296e91a8ace018388941ae1b985
emmalaroudie <laroudie.e@gmail.com>
2018-12-07 18:16:04 +0100

    magie

--- Code/SolutionProjet/Main/Program.cs
+++ Code/SolutionProjet/Main/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using System; using System.Collections.Generic; using System.Linq; using System.Text; using System.Threading.Tasks; using ControleurRestaurant;
+using ModeleRestaurant;
 
-namespace Main
-{
-    class MainClass
+namespace Main {
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Restaurant restaurant = new Restaurant();
+            restaurant.restaurant(); 
         }
-    }
-}
+    } }
